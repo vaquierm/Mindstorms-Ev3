5db949570f0ec1690b6e6817bfb62060c7f1e1dc
vaquierm <michael.vaquier@mail.mcgill.ca>
2017-09-20 11:23:54 -0400

    odometry folder added

--- Wall_Following/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ Wall_Following/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -3,93 +3,90 @@ package ca.mcgill.ecse211.wallfollowing;
 import lejos.hardware.motor.*;
 
 public class BangBangController implements UltrasonicController {
-	
-	  private static final int FILTER_OUT = 35;
-	  private static final int SPIN_SPEED = 175;
 
-  private final int bandCenter;
-  private final int bandwidth;
-  private final int motorLow;
-  private final int motorHigh;
-  private int distance;
-  
-  private int filterControl;
-  private boolean spinning = false;
+	private static final int FILTER_OUT = 35;
+	private static final int SPIN_SPEED = 175;
 
-  public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
-    // Default Constructor
-    this.bandCenter = bandCenter;
-    this.bandwidth = bandwidth;
-    this.motorLow = motorLow;
-    this.motorHigh = motorHigh;
-    WallFollowingLab.leftMotor.setSpeed(motorLow); // Start robot moving forward
-    WallFollowingLab.rightMotor.setSpeed(motorLow);
-    WallFollowingLab.leftMotor.forward();
-    WallFollowingLab.rightMotor.forward();
-  }
+	private final int bandCenter;
+	private final int bandwidth;
+	private final int motorLow;
+	private final int motorHigh;
+	private int distance;
 
-  @Override
-  public void processUSData(int distance) {
-	  
-	if (distance >= 100 && filterControl < FILTER_OUT) {
-      // bad value, do not set the distance var, however do increment the
-      // filter value
-      filterControl++;
-      this.distance = bandCenter; //while waiting for filter control to confirm that there is indeed nothing there, the robot goes straight
-    }
-	 else if (distance >= 100) {
-      // We have repeated large values, so there must actually be nothing
-      // there: leave the distance alone
-      this.distance = distance;
-    } else {
-      // distance went below 255: reset filter and leave
-      // distance alone.
-      filterControl = 0;
-      this.distance = distance;
-    }
-	
-	
-    if (Math.abs(this.distance - bandCenter) < bandwidth) { //Sweet spot
-    	WallFollowingLab.leftMotor.setSpeed(motorLow);
-        WallFollowingLab.rightMotor.setSpeed(motorLow);
-        WallFollowingLab.rightMotor.forward();
-        WallFollowingLab.leftMotor.forward();
-        spinning = false;
-    }
-    else if (this.distance < bandCenter) { //Too close
-    	if (this.distance < 10) { // if the robot is way way too close it backs up fast
-    		WallFollowingLab.leftMotor.setSpeed(300);
-    		WallFollowingLab.rightMotor.setSpeed(300);
-    	    WallFollowingLab.rightMotor.backward();
-    	    WallFollowingLab.leftMotor.backward();
-    	    spinning = true;
-    	}
-    	else if (this.distance < 20) { // if the robot is way too close, it spins
-    		WallFollowingLab.leftMotor.setSpeed(SPIN_SPEED);
-    		WallFollowingLab.rightMotor.setSpeed(SPIN_SPEED);
-    	    WallFollowingLab.rightMotor.backward();
-    	    WallFollowingLab.leftMotor.forward();
-    	    spinning = true;
-    	}
-    	else {
-    		WallFollowingLab.leftMotor.setSpeed(motorHigh);
-    		WallFollowingLab.rightMotor.setSpeed(motorLow);
-    		WallFollowingLab.rightMotor.forward();
-    		WallFollowingLab.leftMotor.forward();
-    		spinning = false;
-    	}
-    }
-    else { //Too far
-        WallFollowingLab.leftMotor.setSpeed(motorLow);
-        WallFollowingLab.rightMotor.setSpeed(motorHigh);
-        WallFollowingLab.rightMotor.forward();
-        WallFollowingLab.leftMotor.forward();
-        spinning = false;
-    }
-  }
+	private int filterControl;
+	private boolean spinning = false;
 
-  @Override
-  public int readUSDistance() {
-    return this.distance;
-  }
+	public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
+		// Default Constructor
+		this.bandCenter = bandCenter;
+		this.bandwidth = bandwidth;
+		this.motorLow = motorLow;
+		this.motorHigh = motorHigh;
+		WallFollowingLab.leftMotor.setSpeed(motorLow); // Start robot moving
+														// forward
+		WallFollowingLab.rightMotor.setSpeed(motorLow);
+		WallFollowingLab.leftMotor.forward();
+		WallFollowingLab.rightMotor.forward();
+	}
+
+	@Override
+	public void processUSData(int distance) {
+
+		if (distance >= 100 && filterControl < FILTER_OUT) {
+			// bad value increment the filter value
+			// set distance to bandCenter to go straight
+			filterControl++;
+			this.distance = bandCenter; // while waiting for filter control to
+										// confirm that there is indeed nothing
+										// there, the robot goes straight
+		} else if (distance >= 100) {
+			// We have repeated large values, so there must actually be nothing
+			this.distance = distance;
+		} else {
+			// distance went below 100: reset filter
+			filterControl = 0;
+			this.distance = distance;
+		}
+
+		if (Math.abs(this.distance - bandCenter) < bandwidth) { // Sweet spot
+			WallFollowingLab.leftMotor.setSpeed(motorLow);
+			WallFollowingLab.rightMotor.setSpeed(motorLow);
+			WallFollowingLab.rightMotor.forward();
+			WallFollowingLab.leftMotor.forward();
+			spinning = false;
+		} else if (this.distance < bandCenter) { // Too close
+			if (this.distance < 10) { // if the robot is way way too close it
+										// backs up fast
+				WallFollowingLab.leftMotor.setSpeed(300);
+				WallFollowingLab.rightMotor.setSpeed(300);
+				WallFollowingLab.rightMotor.backward();
+				WallFollowingLab.leftMotor.backward();
+				spinning = true;
+			} else if (this.distance < 20) { // if the robot is way too close,
+												// it spins
+				WallFollowingLab.leftMotor.setSpeed(SPIN_SPEED);
+				WallFollowingLab.rightMotor.setSpeed(SPIN_SPEED);
+				WallFollowingLab.rightMotor.backward();
+				WallFollowingLab.leftMotor.forward();
+				spinning = true;
+			} else {
+				WallFollowingLab.leftMotor.setSpeed(motorHigh);
+				WallFollowingLab.rightMotor.setSpeed(motorLow);
+				WallFollowingLab.rightMotor.forward();
+				WallFollowingLab.leftMotor.forward();
+				spinning = false;
+			}
+		} else { // Too far
+			WallFollowingLab.leftMotor.setSpeed(motorLow);
+			WallFollowingLab.rightMotor.setSpeed(motorHigh);
+			WallFollowingLab.rightMotor.forward();
+			WallFollowingLab.leftMotor.forward();
+			spinning = false;
+		}
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
 }
--- Wall_Following/src/ca/mcgill/ecse211/wallfollowing/PController.java
+++ Wall_Following/src/ca/mcgill/ecse211/wallfollowing/PController.java
@@ -4,120 +4,111 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class PController implements UltrasonicController {
 
-  /* Constants */
-  private static final int MOTOR_SPEED = 140;
-  private static final int FILTER_OUT = 45;
-  private final int ERROR_PROPORTIONALITY = 35;
-  private final int CORNER_SPEED_DIFFERENCE = 45;
-  private static final int SPIN_SPEED = 150;
+	/* Constants */
+	private static final int MOTOR_SPEED = 140;
+	private static final int FILTER_OUT = 45;
+	private final int ERROR_PROPORTIONALITY = 35;
+	private final int CORNER_SPEED_DIFFERENCE = 45;
+	private static final int SPIN_SPEED = 150;
 
-  private final int bandCenter;
-  private final int bandWidth;
-  private int wheelSpeedDifference;
-  private int distance;
-  private int filterControl;
-  private boolean spinning = false;
-  private boolean corner = false;
+	private final int bandCenter;
+	private final int bandWidth;
+	private int wheelSpeedDifference;
+	private int distance;
+	private int filterControl;
+	private boolean spinning = false;
+	private boolean corner = false;
 
-  public PController(int bandCenter, int bandWidth) {
-    this.bandCenter = bandCenter;
-    this.bandWidth = bandWidth;
-    this.filterControl = 0;
+	public PController(int bandCenter, int bandWidth) {
+		this.bandCenter = bandCenter;
+		this.bandWidth = bandWidth;
+		this.filterControl = 0;
 
-    WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED); // Initalize motor rolling forward
-    WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
-    WallFollowingLab.leftMotor.forward();
-    WallFollowingLab.rightMotor.forward();
-  }
-
-  @Override
-  public void processUSData(int distance) {
-	  
-
-    // rudimentary filter - toss out invalid samples corresponding to null
-    // signal.
-    // (n.b. this was not included in the Bang-bang controller, but easily
-    // could have).
-    //
-	if (distance >= 100 && filterControl < FILTER_OUT) {
-      // bad value, do not set the distance var, however do increment the
-      // filter value
-      filterControl++;
-      this.distance = bandCenter;
-    }
-	else if (distance >= 100) {
-      // We have repeated large values, so there must actually be nothing
-      // there: leave the distance alone
-      this.distance = distance;
-    } else {
-      // distance went below 255: reset filter and leave
-      // distance alone.
-      filterControl = 0;
-      this.distance = distance;
-    }
-	
-	corner = false;
-    if (this.distance >= 100) {
-    	wheelSpeedDifference = CORNER_SPEED_DIFFERENCE;
-    	WallFollowingLab.leftMotor.setSpeed(140);
-        WallFollowingLab.rightMotor.setSpeed(240);
-        WallFollowingLab.rightMotor.forward();
-        WallFollowingLab.leftMotor.forward();
-        corner = true;
-    }
-    else {
-    	wheelSpeedDifference = Math.abs(ERROR_PROPORTIONALITY * (bandCenter - distance)/10);
-    	if(wheelSpeedDifference > 90)
-    		wheelSpeedDifference = 90;
-    }
-    
-	if (!corner) {
-	    if (Math.abs(this.distance - bandCenter) < bandWidth) { //Sweet spot
-	    	WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED);
-	        WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
-	        WallFollowingLab.rightMotor.forward();
-	        WallFollowingLab.leftMotor.forward();
-	    }
-	    else if (this.distance < bandCenter) { //Too close
-	    	if (this.distance < 10) {
-	    		WallFollowingLab.leftMotor.setSpeed(300);
-	    		WallFollowingLab.rightMotor.setSpeed(300);
-	    		WallFollowingLab.rightMotor.backward();
-	    		WallFollowingLab.leftMotor.backward();
-	    		spinning = false;
-	    	}
-	    	else if (this.distance < 20) { // if the robot is way too close, it spins
-	    		WallFollowingLab.leftMotor.setSpeed(SPIN_SPEED);
-	    		WallFollowingLab.rightMotor.setSpeed(SPIN_SPEED);
-	    	    WallFollowingLab.rightMotor.backward();
-	    	    WallFollowingLab.leftMotor.forward();
-	    	    spinning = true;
-	    	}
-	    	else {
-	    		WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED + wheelSpeedDifference);
-	    		WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED - wheelSpeedDifference);
-	    		WallFollowingLab.rightMotor.forward();
-	    		WallFollowingLab.leftMotor.forward();
-	    		spinning = false;
-	    	}
-	    }
-	    else { //Too far
-	        WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED - wheelSpeedDifference);
-	        WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED + wheelSpeedDifference);
-	        WallFollowingLab.rightMotor.forward();
-	        WallFollowingLab.leftMotor.forward();
-	        spinning = false;
-	    }
+		WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED); // Initalize motor
+															// rolling forward
+		WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
+		WallFollowingLab.leftMotor.forward();
+		WallFollowingLab.rightMotor.forward();
 	}
 
-      
-    
-  }
+	@Override
+	public void processUSData(int distance) {
 
+		// rudimentary filter - toss out invalid samples corresponding to null
+		// signal.
+		// (n.b. this was not included in the Bang-bang controller, but easily
+		// could have).
+		//
+		if (distance >= 100 && filterControl < FILTER_OUT) {
+			// bad value increment the filter value
+			// set distance to bandCenter to go straight
+			filterControl++;
+			this.distance = bandCenter;
+		} else if (distance >= 100) {
+			// We have repeated large values, so there must actually be nothing
+			this.distance = distance;
+		} else {
+			// distance went below 100: reset filter
+			filterControl = 0;
+			this.distance = distance;
+		}
 
-  @Override
-  public int readUSDistance() {
-    return this.distance;
-  }
+		corner = false;
+		if (this.distance >= 100) {
+			wheelSpeedDifference = CORNER_SPEED_DIFFERENCE;
+			WallFollowingLab.leftMotor.setSpeed(140);
+			WallFollowingLab.rightMotor.setSpeed(240);
+			WallFollowingLab.rightMotor.forward();
+			WallFollowingLab.leftMotor.forward();
+			corner = true;
+		} else {
+			wheelSpeedDifference = Math.abs(ERROR_PROPORTIONALITY * (bandCenter - distance) / 10);
+			if (wheelSpeedDifference > 90)
+				wheelSpeedDifference = 90;
+		}
+
+		if (!corner) {
+			if (Math.abs(this.distance - bandCenter) < bandWidth) { // Sweet
+																	// spot
+				WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED);
+				WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
+				WallFollowingLab.rightMotor.forward();
+				WallFollowingLab.leftMotor.forward();
+			} else if (this.distance < bandCenter) { // Too close
+				if (this.distance < 10) {
+					WallFollowingLab.leftMotor.setSpeed(300);
+					WallFollowingLab.rightMotor.setSpeed(300);
+					WallFollowingLab.rightMotor.backward();
+					WallFollowingLab.leftMotor.backward();
+					spinning = false;
+				} else if (this.distance < 20) { // if the robot is way too
+													// close, it spins
+					WallFollowingLab.leftMotor.setSpeed(SPIN_SPEED);
+					WallFollowingLab.rightMotor.setSpeed(SPIN_SPEED);
+					WallFollowingLab.rightMotor.backward();
+					WallFollowingLab.leftMotor.forward();
+					spinning = true;
+				} else {
+					WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED + wheelSpeedDifference);
+					WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED - wheelSpeedDifference);
+					WallFollowingLab.rightMotor.forward();
+					WallFollowingLab.leftMotor.forward();
+					spinning = false;
+				}
+			} else { // Too far
+				WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED - wheelSpeedDifference);
+				WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED + wheelSpeedDifference);
+				WallFollowingLab.rightMotor.forward();
+				WallFollowingLab.leftMotor.forward();
+				spinning = false;
+			}
+		}
+
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
 
 }
